837338faf4fccea253a120448bcbe00458219253
jahid45780 <jahidhossain88899@gmail.com>
2023-10-23 02:50:08 +0600

    add the page

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
       
         <footer className="footer p-10 bg-base-200 text-base-content">
   <aside>
-      <img className=" w-40 h-36" src="https://i.ibb.co/SXHqMYF/32378241-7932097.jpg" alt="" />
+      <img className=" w-40 h-36" src="https://i.ibb.co/NtvTp4K/395199656-859942578832265-6048415189227583555-n.jpg" alt="" />
     <p>When you purchase a new vehicle in the United States<br/>You receive a title whether you buy a new or used vehicle from a dealer or a private citizen.</p>
   <div className=" grid grid-cols-3 gap-3" >
   <a href=""> <AiFillFacebook className=" text-5xl rounded-full " ></AiFillFacebook> </a>
@@ -13,7 +13,9 @@ const Footer = () => {
     <a href=""> <AiFillTwitterCircle className=" text-5xl rounded-full "  ></AiFillTwitterCircle> </a>
   </div>
   </aside> 
-  <nav>
+ <div className=" footer grid grid-cols-3 mt-14 mr-36" >
+
+ <nav>
     <header className="footer-title">Services</header> 
     <a className="link link-hover">Branding</a> 
     <a className="link link-hover">Design</a> 
@@ -33,6 +35,9 @@ const Footer = () => {
     <a className="link link-hover">Privacy policy</a> 
     <a className="link link-hover">Cookie policy</a>
   </nav>
+
+
+ </div>
 </footer>
 
 
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "./LayOut/Main";
 import Home from "./Pages/Home/Home/Home";
+import ErrorPage from "./Pages/Shared/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement:<ErrorPage></ErrorPage>,
       children:[
         {
             path:'/',
